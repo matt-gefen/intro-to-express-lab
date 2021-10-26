a55484f903cb410397b45ea7b65aeedf08cada82
mattgefen <matt.gefen@gmail.com>
2021-10-26 08:12:51 -0600

    reorder

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.get('/', function(req, res) {
+  res.redirect('/home')
+})
+
 app.get('/home', function(req, res) {
   // app.use(express.static(__dirname + '/home'))
   res.render('home')
-})
-
-app.get('/', function(req, res) {
-  res.redirect('/home')
 })
 
 app.get('/bo-lyrics', function(req, res) {
