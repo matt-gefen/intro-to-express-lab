de1bfbf54749f39eaa9cd1dd034873de5e93ad96
mattgefen <matt.gefen@gmail.com>
2021-10-26 07:18:01 -0600

    added data file

---
+++ data/bo-lyrics-db.js
@@ -0,0 +1,5 @@
+const todos = [
+  {title: 'Content', lyric: 'But look I made you some content!', playing: true, _id: 12345},
+  {title: 'Comedy', lyric: 'If you start to smell burnt toast you\'re having a stroke or overcooking your toast.',playing: false, _id: 67891},
+  {title: 'Welcome to the Internet', lyric:'Welcome to the internet! Hold on to your socks!' ,playing: false, _id: 56789},
+]
